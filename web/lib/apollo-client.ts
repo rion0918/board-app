dfd874f42bb7062792df074b79f296bd7f5d5961
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-05-12 10:41:37 +0900

    vercel修正

--- web/lib/apollo-client.ts
+++ web/lib/apollo-client.ts
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:3900/graphql',
+    uri:
+      process.env.NEXT_PUBLIC_GRAPHQL_API_URL ||
+      'http://localhost:3900/graphql',
     credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
