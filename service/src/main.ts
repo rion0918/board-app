dc8be4cf419db33c1eb89e87bb6fef4e49cd62de
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-05-12 11:28:53 +0900

    render

--- service/src/main.ts
+++ service/src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://board-app-phi.vercel.app'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3900;
   await app.listen(port);
   console.log(`🚀 Server is running on http://localhost:${port}`);
